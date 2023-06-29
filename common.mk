c9ff73b7ca504d43d9b01b2c6596e0b64cfac8d7
Luofan Chen <clfbbn@gmail.com>
2023-06-29 20:30:32 +0000

    sm8150-common: Build lineage health HAL

--- common.mk
+++ common.mk
@@ -285,6 +285,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.lenovo_msmnile
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
